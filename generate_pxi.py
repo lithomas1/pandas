36112effe154c54ade294eb48677dd8876f5f658
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2022-07-11 21:46:04 -0700

    WIP: Build pandas with meson

---
+++ generate_pxi.py
@@ -0,0 +1,40 @@
+import argparse
+import os
+
+from Cython import Tempita
+
+
+def process_tempita(pxifile, outfile):
+    if (
+        os.path.exists(outfile)
+        and os.stat(pxifile).st_mtime < os.stat(outfile).st_mtime
+    ):
+        # if .pxi.in is not updated, no need to output .pxi
+        return
+
+    with open(pxifile) as f:
+        tmpl = f.read()
+    pyxcontent = Tempita.sub(tmpl)
+
+    with open(outfile, "w") as f:
+        f.write(pyxcontent)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("infile", type=str, help="Path to the input file")
+    parser.add_argument("-o", "--outdir", type=str, help="Path to the output directory")
+    args = parser.parse_args()
+
+    if not args.infile.endswith(".in"):
+        raise ValueError(f"Unexpected extension: {args.infile}")
+
+    outdir_abs = os.path.join(os.getcwd(), args.outdir)
+    outfile = os.path.join(
+        outdir_abs, os.path.splitext(os.path.split(args.infile)[1])[0]
+    )
+
+    process_tempita(args.infile, outfile)
+
+
+main()
