e94cb1ad5edd0c00fca2e3e12cca7838c0ad04c5
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2022-08-22 14:47:02 -0700

    update?

--- ci/test_wheels.sh
+++ ci/test_wheels.sh
@@ -1,17 +1,32 @@
 #!/bin/bash
 PYTHON_VERSION=$(python -c "import platform; print(platform.python_version())")
-if [[ ${{ env.IS_32_BIT }} ]]; then
+if [[ $IS_32_BIT ]]; then
+    if [[ $RUNNER_OS == "Windows" ]]; then
+      exit 0
+    fi
     docker pull quay.io/pypa/manylinux2014_i686
     docker run --platform linux/386 -v $(pwd):/pandas quay.io/pypa/manylinux2014_i686 \
     /bin/bash -xc "python --version \
     pip install pytz six numpy python-dateutil \
     pip install --find-links=pandas/pandas/dist --no-index pandas \
-    python -c 'import pandas as pd; 
+    python -c 'import pandas as pd;
                 print(pd.__version__);
                 pandas.test(extra_args=["-m not clipboard and not single_cpu", "--skip-slow", "--skip-network", "--skip-db", "-n=2"]);
                 pandas.test(extra_args=["-m not clipboard and single_cpu", "--skip-slow", "--skip-network", "--skip-db"])'"
 else
-    python -c "import pandas; print(pandas.__version__);
-    pandas.test(extra_args=['-m not clipboard and not single_cpu', '--skip-slow', '--skip-network', '--skip-db', '-n=2']);
-    pandas.test(extra_args=['-m not clipboard and single_cpu', '--skip-slow', '--skip-network', '--skip-db'])"
-endif
+    if [[ $RUNNER_OS == "Windows" ]]; then
+      docker pull python:$PYTHON_VERSION-windowsservercore
+      docker run -v $(pwd):/pandas python:$PYTHON_VERSION-windowsservercore \
+      cmd.exe "python --version &&
+              pip install pytz six numpy python-dateutil &&
+              pip install --find-links=pandas/pandas/dist --no-index pandas &&
+              python -c 'import pandas as pd;
+                          print(pd.__version__);
+                          pandas.test(extra_args=["-m not clipboard and not single_cpu", "--skip-slow", "--skip-network", "--skip-db", "-n=2"]);
+                          pandas.test(extra_args=["-m not clipboard and single_cpu", "--skip-slow", "--skip-network", "--skip-db"])'"
+    else
+      python -c "import pandas; print(pandas.__version__);
+      pandas.test(extra_args=['-m not clipboard and not single_cpu', '--skip-slow', '--skip-network', '--skip-db', '-n=2']);
+      pandas.test(extra_args=['-m not clipboard and single_cpu', '--skip-slow', '--skip-network', '--skip-db'])"
+    fi
+fi
