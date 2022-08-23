488b32bf54052fccf5a516fbcc4a72a110f1f1a6
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2022-08-23 10:38:09 -0700

    update?

--- ci/test_wheels.sh
+++ ci/test_wheels.sh
@@ -18,7 +18,7 @@ else
     if [[ $RUNNER_OS == "Windows" ]]; then
       docker pull python:$PYTHON_VERSION-windowsservercore
       echo $PROJECT_DIR
-      docker run -v $PROJECT_DIR:/pandas python:$PYTHON_VERSION-windowsservercore /pandas/ci/test_wheels_windows.bat
+      docker run -v $PROJECT_DIR:c:\pandas python:$PYTHON_VERSION-windowsservercore /pandas/ci/test_wheels_windows.bat
     else
       python -c "import pandas; print(pandas.__version__);
 pandas.test(extra_args=['-m not clipboard and not single_cpu', '--skip-slow', '--skip-network', '--skip-db', '-n=2']);
