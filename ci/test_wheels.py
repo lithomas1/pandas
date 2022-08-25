69313071a8dc8ba85350170cfad5f5a8e8e7ffb0
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2022-08-24 17:02:52 -0700

    more debugging

--- ci/test_wheels.py
+++ ci/test_wheels.py
@@ -4,13 +4,16 @@ import sys
 import subprocess
 py_ver = platform.python_version()
 is_32_bit = bool(os.getenv('IS_32_BIT'))
+print(f"IS_32_BIT is {is_32_bit}")
 if os.name == "nt":
     if is_32_bit:
         sys.exit(0) # No way to test Windows 32-bit(no docker image)
-    subprocess.run(f"docker pull python:{PYTHON_VERSION}-windowsservercore", check=True)
+    print(f"Pulling docker image to test Windows 64-bit Python {py_ver}")
+    subprocess.run(f"docker pull python:{py_ver}-windowsservercore", check=True)
     pandas_base_dir = os.path.join(os.path.dirname(__file__, '..'))
+    print(f"pandas project dir is {pandas_base_dir}")
     subprocess.run(f'cd {pandas_base_dir} && docker run -v "`pwd`:`pwd`" -w `pwd` '
-                   f'python:{PYTHON_VERSION}-windowsservercore ci/test_wheels_windows.bat', check=True)
+                   f'python:{py_ver}-windowsservercore ci/test_wheels_windows.bat', check=True)
 else:
     import pandas as pd
     pandas.test(extra_args=['-m not clipboard and not single_cpu', '--skip-slow', '--skip-network', '--skip-db', '-n=2'])
