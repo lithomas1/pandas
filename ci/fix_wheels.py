6c4627df2dcee78c3e1f36e07219ad1a88470c89
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2022-08-27 06:18:49 -0700

    add missing dlls

---
+++ ci/fix_wheels.py
@@ -0,0 +1,36 @@
+import shutil
+import zipfile
+try:
+    _, wheel_path, dest_dir, is_32 = sys.argv
+    PYTHON_ARCH = "x86" if is32 else "x64"
+except ValueError:
+    # Too many/little values to unpack
+    raise ValueError("User must pass in the path to the wheel"
+                    "destination directory, and whether the Python is 32/64-bit"
+                    "for the repaired wheel.")
+# Wheels are zip files
+if not os.path.isdir(dest_dir):
+    print(f"Created directory {dest_dir}")
+    os.mkdir(dest_dir)
+shutil.copy(wheel_path, dest_dir) # Remember to delete if process fails
+wheel_name = os.path.basename(wheel_path)
+success = True
+exception = None
+repaired_wheel_path = os.path.join(dest_dir, wheel_name)
+with zipfile.ZipFile(repaired_wheel_path, "a") as zipf:
+    try:
+        # TODO: figure out how licensing works for the redistributables
+        base_redist_dir = f"C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Redist/MSVC/14.29.30133/{PYTHON_ARCH}/Microsoft.VC142.CRT/"
+        zipf.write(os.path.join(base_redist_dir, "msvcp140.dll"), "pandas/_libs/window")
+        zipf.write(os.path.join(base_redist_dir, "concrt140.dll"), "pandas/_libs/window")
+        zipf.write(os.path.join(base_redist_dir, "vcruntime140_1.dll"), "pandas/_libs/window")
+    except Exception as e:
+        success = False
+        exception = e
+
+if not success:
+    os.remove(repaired_wheel_path)
+    raise exception
+else:
+    print(f"Successfully repaired wheel was written to {repaired_wheel_path}")
+    
