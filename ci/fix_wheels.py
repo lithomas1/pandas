811df9d5fceb0bab8073e425dd3a02cd8298bb7e
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2022-08-27 08:25:04 -0700

    fix filenames?

--- ci/fix_wheels.py
+++ ci/fix_wheels.py
@@ -26,10 +26,10 @@ with zipfile.ZipFile(repaired_wheel_path, "a") as zipf:
     try:
         # TODO: figure out how licensing works for the redistributables
         base_redist_dir = f"C:/Program Files (x86)/Microsoft Visual Studio/2019/Enterprise/VC/Redist/MSVC/14.29.30133/{PYTHON_ARCH}/Microsoft.VC142.CRT/"
-        zipf.write(os.path.join(base_redist_dir, "msvcp140.dll"), "pandas/_libs/window")
-        zipf.write(os.path.join(base_redist_dir, "concrt140.dll"), "pandas/_libs/window")
+        zipf.write(os.path.join(base_redist_dir, "msvcp140.dll"), "pandas/_libs/window/msvcp140.dll")
+        zipf.write(os.path.join(base_redist_dir, "concrt140.dll"), "pandas/_libs/window/concrt140.dll")
         if not is_32:
-            zipf.write(os.path.join(base_redist_dir, "vcruntime140_1.dll"), "pandas/_libs/window")
+            zipf.write(os.path.join(base_redist_dir, "vcruntime140_1.dll"), "pandas/_libs/window/vcruntime140_1.dll")
     except Exception as e:
         success = False
         exception = e
